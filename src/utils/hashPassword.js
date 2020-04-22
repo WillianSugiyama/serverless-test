4ff1d99ecfbb18b53282b54d1de9c8a5f6b2b4fe
willian sugiyama <williansugiyama@gmail.com>
2020-04-22 05:51:55 -0300

    fix: refactor hashpassword to es6

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -1,11 +1,11 @@
-const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 
-module.exports = async function (password) {
+export default async function (password) {
   try {
     const hashPass = await bcrypt.hash(password, 10);
 
     return hashPass;
   } catch (error) {
-    throw new Error();
+    throw new Error(error);
   };
-};
+}
